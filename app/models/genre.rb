637ade04c97a2f971773513cc0f39dec21f1b4af
Tim Knoll <timothy.knoll@gmail.com>
2019-08-27 02:26:04 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -15,6 +15,6 @@ class Genre < ActiveRecord::Base
   def all_artist_names
     # return an array of strings containing every musician's name
     artist = self.artists.collect do |artist|
-      artist.name
+      artist.name 
   end
 end

911d529a8c811fc84c640bd25012d51f1ad62af5
Tim Knoll <timothy.knoll@gmail.com>
2019-08-27 02:23:31 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -14,8 +14,8 @@ class Artist < ActiveRecord::Base
 
   def genre_count
     #return the number of genres associated with the artist
-    genres = self.songs.collect do |song|
-      song.genres 
+    genre = self.songs.collect do |song|
+      song.genre 
     end
     genres.compact.count
   end
